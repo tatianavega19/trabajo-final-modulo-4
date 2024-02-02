c101dcdf5aa68b409c6287e24fc8af3bf73f0ea2
tatianavega <tatiana.vega1@hotmail.com>
2024-02-01 22:22:18 -0300

    Creacion del metodo getUrlById e interface.

---
+++ src/interface/mangaInterface.ts
@@ -0,0 +1,19 @@
+interface AnimeCharacter {
+    mal_id: number;
+    url: string;
+    images: {
+        jpg: {
+            image_url: string;
+        };
+        webp: {
+            image_url: string;
+        };
+    };
+    name: string;
+    name_kanji: string;
+    nicknames: string[];
+    favorites: number;
+    about: string;
+}
+
+export {AnimeCharacter}
--- src/model/animeModel.ts
+++ src/model/animeModel.ts
@@ -1,10 +1,11 @@
+import { AnimeCharacter } from "../interface/mangaInterface";
 import dotenv from "dotenv";
 dotenv.config();
 
 const urlApi = process.env.ANIME_BASE_API!;
 const api = new URL(urlApi);
 
-const getAllData = async () => {
+const getAllData = async (): Promise<AnimeCharacter[] | Error> => {
     try {
         const response = await fetch(api);
         if (response.status === 404) {
@@ -14,14 +15,46 @@ const getAllData = async () => {
         const usefulinformation = responseData.data;
 
         return usefulinformation
-    } catch (error) {
+    } catch (error: any) {
         return error
     };
 };
 
+const getUrlById = async (id: number) => {
+    try {
+        const data = await getAllData()
+
+        if (data instanceof Error) {
+            throw data
+        }
+
+        const character = data.find((character) => character.mal_id === id);
+
+        if (!character) {
+            throw new Error(`Character with ID ${id} not found`);
+        }
+
+        const characterInfo = {
+            anime: character.name,
+            url: character.url,
+        };
+
+        const animeString =
+            `  Character's name: ${characterInfo.anime}, 
+    URL: ${characterInfo.url}`;
+
+        return animeString;
+    } catch (error) {
+        return error
+    }
+};
+
+
 const main = async () => {
-    const data = await getAllData();
+    const data = await getUrlById(417);
     console.log(data)
 };
 
 main()
+
+export {getAllData}
